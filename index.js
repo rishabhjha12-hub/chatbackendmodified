49f604f0d580cadbd275803fd94a4e631be3a87d
Rishabh <rishabhjha0@gmail.com>
2023-02-09 18:27:25 +0530

    added username

--- index.js
+++ index.js
@@ -39,6 +39,46 @@
 
 // console.log('listening to port 8000');
 // app.listen(8000)
+
+
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+// const events = require('events');
+// const eventEmitter = new events.EventEmitter();
+// const cors = require('cors')
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.json());
+
+// app.use((req, res, next) => {
+//       res.header('Access-Control-Allow-Origin', '*');
+//       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//       next();
+// });
+
+// app.get('/stream', (req, res) => {
+//   res.set({
+//     'Content-Type': 'text/event-stream',
+//     'Cache-Control': 'no-cache',
+//     'Connection': 'keep-alive'
+//   });
+//   eventEmitter.on('message', (message) => {
+//     res.write(`data: ${message}\n\n`);
+//   });
+// });
+
+// app.post('/message', (req, res) => {
+//   eventEmitter.emit('message', req.body.message);
+//   res.status(201).send({ message: 'Message created successfully' });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running at http://localhost:${port}/`);
+// });
+
+
+
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -49,9 +89,9 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-      next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
 });
 
 app.get('/stream', (req, res) => {
@@ -66,7 +106,8 @@ app.get('/stream', (req, res) => {
 });
 
 app.post('/message', (req, res) => {
-  eventEmitter.emit('message', req.body.message);
+  const { username, message } = req.body;
+  eventEmitter.emit('message', `${username}: ${message}`);
   res.status(201).send({ message: 'Message created successfully' });
 });
 
